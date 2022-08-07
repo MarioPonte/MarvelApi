e5ace7f08db4e8e358fe37ecb4b352560830e3d8
Mário Ponte <ponteolavo30@gmail.com>
2022-08-07 23:24:44 +0100

    edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
 // Request api da marvel
 
 const url = "https://gateway.marvel.com/v1/public/characters?apikey=13211d1e170e426bd4688717129310e6&ts=1&hash=95520260e855ed5e627a8f270f9c9505";
@@ -8,7 +16,10 @@ async function getDados() {
 
     const pegueiDados = JSON.parse(JSON.stringify(response));
 
-    console.log(pegueiDados);
+    for(var i=0; i<(pegueiDados.data.results).length; i++){
+        console.log(pegueiDados.data.results[i].name);
+        $('#infoSection').append('<div class="heroInfo"><img src="' + pegueiDados.data.results[i].thumbnail.path + '.' + pegueiDados.data.results[i].thumbnail.extension + '" alt="" srcset="" class="heroImg"><h2>' + pegueiDados.data.results[i].name + '</h2><p>' + pegueiDados.data.results[i].description + '</p></div>');
+    }
 
 }
 getDados();
